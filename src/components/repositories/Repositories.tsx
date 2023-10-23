0d26825fc82c2ef7e3532ddedd95176dcf95b22a
Octavio Lara <octavioasapchi@gmail.com>
2023-10-23 13:05:12 -0400

    UPDATE use local github

--- src/components/repositories/Repositories.tsx
+++ src/components/repositories/Repositories.tsx
@@ -1,33 +1,30 @@
 import React from "react";
 import Link from "next/link";
-async function getRepos() {
-  const response = await fetch(process.env.NEXT_PUBLIC_API_URL + "/github/", {
-    next: { revalidate: 3600 },
-  });
-  const data = await response.json();
-  return data.repositories;
-}
+import useGithub from "@/hooks/useGithub";
+
 const Repositories = async () => {
-  const repos = await getRepos();
+  const { getCurrentRepos } = useGithub();
+  const repos = await getCurrentRepos();
   return (
     <div className="border-2 border-black p-3 shadow-brutal">
       <div className="flex justify-between items-center">
         <h2 className="text-2xl font-bold">Repositorios</h2>
-        {repos.length}
+        {repos && repos.length}
       </div>
-      {repos.map((repo: any) => {
-        return (
-          <div key={repo.name} className="border-b-2 border-black pb-2 my-2">
-            <h3>{repo.name.replace("-", " ").toUpperCase()}</h3>
-            <Link
-              className="text-sm underline hover:font-bold"
-              href={`${repo.url}`}
-            >
-              Ver en github
-            </Link>
-          </div>
-        );
-      })}
+      {repos &&
+        repos.map((repo: any) => {
+          return (
+            <div key={repo.name} className="border-b-2 border-black pb-2 my-2">
+              <h3>{repo.name.replace("-", " ").toUpperCase()}</h3>
+              <Link
+                className="text-sm underline hover:font-bold"
+                href={`${repo.url}`}
+              >
+                Ver en github
+              </Link>
+            </div>
+          );
+        })}
     </div>
   );
 };
