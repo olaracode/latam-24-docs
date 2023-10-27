9f522874ed1ca6c6795eccfe1656b618eb103494
Octavio Lara <octavioasapchi@gmail.com>
2023-10-27 18:15:06 -0400

    UPDATE markdown styles

--- src/components/mics/MarkDown.tsx
+++ src/components/mics/MarkDown.tsx
@@ -7,7 +7,8 @@ import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 const h1Styles = "[&>h1]:text-2xl [&>h1]:font-bold [&>h1]:border-b-2";
 const h2Styles =
   "[&>h2]:text-xl [&>h2]:font-bold [&>h2]:border-b-2 [&>h2]:my-3";
-
+const h3Styles =
+  "[&>h3]:text-lg [&>h3]:font-bold [&>h3]:mt-5 [&>h3]:mb-1 [&>h3]:border-b-2";
 const ulStyles = "[&>ul]:list-disc [&>ul]:ml-5 [&>ul]:my-3";
 const olStyles = "[&>ol]:list-decimal [&>ol]:ml-5 [&>ol] my-3:";
 const liStyles = "[&>li]:my-2";
@@ -16,7 +17,7 @@ const blockquoteStyles =
 const MarkDown = ({ content }: { content: string }) => {
   return (
     <div
-      className={`${h1Styles} ${h2Styles} ${ulStyles} ${olStyles} ${liStyles} ${blockquoteStyles}`}
+      className={`${h1Styles} ${h2Styles} ${ulStyles} ${olStyles} ${liStyles} ${blockquoteStyles} ${h3Styles}`}
     >
       <ReactMarkdown
         components={{
